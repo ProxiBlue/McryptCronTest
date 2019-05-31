5ef39a72f0021bd71dc06012ee47b7197eaa2568
Lucas <lucas.vansatden@enjo.com.au>
2019-05-31 03:08:33 +0000

    add in more debug data

--- src/app/code/community/ProxiBlue/McryptCronTest/Model/Cron.php
+++ src/app/code/community/ProxiBlue/McryptCronTest/Model/Cron.php
@@ -13,10 +13,28 @@ class ProxiBlue_McryptCronTest_Model_Cron
     public static function run($schedule)
     {
         try {
+            // test if method exists in this version of php
+            if (!function_exists('phpseclib_mcrypt_list_algorithms')) {
+                Mage::log('phpseclib_mcrypt_list_algorithms does not exist' );
+            } else {
+                Mage::log('phpseclib_mcrypt_list_algorithms EXISTS!' );
+            }
+            if(!file_exists('/tmp/phpinfo_cron')) {
+                ob_start();
+                phpinfo();
+                $info = ob_get_contents();
+                ob_end_clean();
+
+                $fp = fopen('/tmp/phpinfo_cron', "w+");
+                fwrite($fp, $info);
+                fclose($fp);
+            }
+
             $test = Mage::getModel('core/encryption')->encrypt('foo-bar');
             Mage::log('Encrypt test: ' . $test);
             $test = Mage::getModel('core/encryption')->decrypt($test);
             Mage::log('Decrypt test: ' . $test);
+
         } catch (Exception $e) {
             // save any errors.
             Mage::logException($e);
